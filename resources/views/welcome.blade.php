5904578ea979f7f30c8d6a6d0a78b33894fd16fb
kouki.t <errroon@yahoo.co.jp>
2020-12-27 07:45:55 +0000

    set timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ★Laravel
                 </div>
 
                 <div class="links">
