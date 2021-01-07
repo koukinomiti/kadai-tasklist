b11904731cbf389e74e175fbf6561d96bf107ca2
kouki.t <errroon@yahoo.co.jp>
2021-01-07 08:22:25 +0000

    saisaidepuroi

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,6 +10,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>タイトル</th>
                     <th>タスク</th>
                 </tr>
             </thead>
